5ed163fe1f9adf7359fff1deb65f88900d6a7881
yap9393 <122944842+yap9393@users.noreply.github.com>
2023-03-03 15:29:05 -0300

    cambios varios

--- myScript.js
+++ myScript.js
@@ -2,21 +2,8 @@
 function modoClaroOscuro() {
     var elementBody = document.body;
     elementBody.classList.toggle("modoClaroOscuro")
-    
-    // if (elementBody.classList=="modoClaroOscuro"){
-    //     document.querySelectorAll("a").forEach(a => {
-    //         a.setAttribute("color", "#c0c0c0");
-    //      });
-    // } else{
-    //     document.querySelectorAll("a").forEach(a => {
-    //         a.setAttribute("color", "#000000");
-        
-    //      });
-    // }
 
 }
- 
-   
  
 //VOLVER ARRIBA
 const toTop=()=>window.scrollTo({top:0, behavior:"smooth"});
