9b47f40be0b52a8b5be6725f7f3e2a42a8188116
yap9393 <122944842+yap9393@users.noreply.github.com>
2023-03-04 13:49:55 -0300

    commit final
    el boton para descargar solo permite descarga cuando se esta en modo claro para evitar problemas de lectura.

--- myScript.js
+++ myScript.js
@@ -13,8 +13,17 @@ const toTop=()=>window.scrollTo({top:0, behavior:"smooth"});
   function mostrarFecha() {
     var d = new Date();
     var dia = d.getDate();
-    var mes = d.getMonth() + 1; //months are zero based
+    var mes = d.getMonth() + 1; 
     var anio = d.getFullYear();
     document.write(dia + "-" + mes + "-" + anio);
   }
 
+//IMPRESION Y DESCARGA SOLO POSIBLE EN MODO CLARO
+function descargarPDF(){
+    var elementBody = document.body;
+if (elementBody.classList.contains('modoClaroOscuro')){
+    alert('Se recomienda descargar e imprimir el sitio en modo claro')
+    } else{
+        window.print()
+    }
+}
