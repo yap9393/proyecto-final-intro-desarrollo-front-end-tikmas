f2067bb7fc1403ef2db3b3398e36e4a2915505fa
yap9393 <122944842+yap9393@users.noreply.github.com>
2023-03-04 13:19:57 -0300

    upload final
    con funcion para descargar pdf y footer con nombre mail y fecha

--- myScript.js
+++ myScript.js
@@ -1,4 +1,4 @@
-//modo oscuro 
+//MODO OSCURO
 function modoClaroOscuro() {
     var elementBody = document.body;
     elementBody.classList.toggle("modoClaroOscuro")
@@ -7,3 +7,14 @@ function modoClaroOscuro() {
  
 //VOLVER ARRIBA
 const toTop=()=>window.scrollTo({top:0, behavior:"smooth"});
+
+
+//FECHA DE HOY
+  function mostrarFecha() {
+    var d = new Date();
+    var dia = d.getDate();
+    var mes = d.getMonth() + 1; //months are zero based
+    var anio = d.getFullYear();
+    document.write(dia + "-" + mes + "-" + anio);
+  }
+
